db6fcb8a24c7b020ba58c4c4e30bfbca18b6f28d
bartahunor <barta.hunor.attila@diak.szbi-pg.hu>
2026-01-16 10:42:37 +0100

    több nap ellenőrzése

--- features/steps/step_definitions.py
+++ features/steps/step_definitions.py
@@ -2,9 +2,9 @@ from behave import given, when, then
 from src.is_it_friday import is_it_friday
 
 
-@given('today is ')
-def step_given_today_is(context):
-    pass
+@given('today is "{day}"')
+def step_given_today_is(context, day):
+    context.today = day
 
 
 @when("I ask whether it's Friday yet")
